5e6ad06959ac87eea5357c7844e8a59c8e0af0c4
RomanHyde <romanhyde1@gmail.com>
2021-06-22 19:33:42 +0100

    add inspiration api

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 const Discord = require("discord.js");
+const fetch = require("node-fetch");
 const client = new Discord.Client();
+
+let getQuote = () => {
+    return fetch("https://zenquotes.io/api/random")
+    .then(res => {
+        return res.json()
+    })
+    .then(data => {
+        // q = quote and a = author
+        return data[0]["q"] + " -" + data[0] ["a"]
+    })
+};
 
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}!`)
-});
+})
 
 client.on("message", msg => {
-    if (msg.content === "ping") {
-        msg.reply("pong")
-    }
-});
+   if (msg.author.bot) return 
+   
+   if (msg.content === "$inspire") {
+    getQuote().then(quote => msg.channel.send(quote))
+   }
+})
 
-client.login(process.env.TOKEN)
+client.login(process.env.TOKEN);
